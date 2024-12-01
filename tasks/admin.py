9f39b46a4c2c2b28c0088a6b241176ad508af7df
Victor Chagas <victor.chagas@itmss.com.br>
2024-12-01 18:35:34 -0300

    feat: admin for users and tasks

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from tasks.models import Task
 
-# Register your models here.
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "owner", "status", "active", "created_at", "updated_at", "expires_at")
+    list_filter = ("status", "active", "created_at", "updated_at", "expires_at")
+    search_fields = ("title", "description", "owner__email", "owner__username")
+    ordering = ("-created_at",)
+    readonly_fields = ("created_at", "updated_at")
+    fieldsets = (
+        (None, {
+            "fields": ("owner", "title", "description", "status", "active", "expires_at")
+        }),
+        ("Timestamps", {
+            "fields": ("created_at", "updated_at"),
+        }),
+    )
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from users.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ("id", "email", "name", "is_staff", "is_active", "created_at", "updated_at")
+    list_filter = ("is_staff", "is_active", "created_at", "updated_at")
+    search_fields = ("email", "name")
+    ordering = ("-created_at",)
+    readonly_fields = ("created_at", "updated_at")
+
+    fieldsets = (
+        (None, {
+            "fields": ("email", "password")
+        }),
+        ("Personal Info", {
+            "fields": ("name",)
+        }),
+        ("Permissions", {
+            "fields": ("is_staff", "is_active", "is_superuser", "groups", "user_permissions"),
+        }),
+        ("Timestamps", {
+            "fields": ("created_at", "updated_at"),
+        }),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "name", "password1", "password2", "is_staff", "is_active"),
+        }),
+    )
