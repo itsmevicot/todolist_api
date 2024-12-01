158abaeedfe7e342dc4d30d390e07eccc676f69d
Victor Chagas <victor.chagas@itmss.com.br>
2024-12-01 18:19:09 -0300

    feat: tests for logger in celery tasks

--- unit_tests/tasks/test_celery_tasks.py
+++ unit_tests/tasks/test_celery_tasks.py
@@ -79,3 +79,32 @@ def test_mark_expired_tasks_logs_error(mock_logger, mock_get_expired_tasks):
         mark_expired_tasks()
 
     mock_logger.error.assert_called_with(f"Failed to mark task ID {mock_task.id} as expired: Update failure")
+
+
+@pytest.mark.django_db
+@patch("tasks.tasks.TaskRepository.get_tasks_expiring_soon",
+       side_effect=Exception("Outer exception in send_expiry_reminder"))
+@patch("tasks.tasks.logger")
+def test_send_expiry_reminder_outer_exception(mock_logger, mock_get_tasks_expiring_soon):
+    """
+    Test handling of an outer exception in send_expiry_reminder.
+    """
+    send_expiry_reminder()
+
+    mock_logger.error.assert_called_with(
+        "An error occurred in send_expiry_reminder: Outer exception in send_expiry_reminder"
+    )
+
+
+@pytest.mark.django_db
+@patch("tasks.tasks.TaskRepository.get_expired_tasks", side_effect=Exception("Outer exception in mark_expired_tasks"))
+@patch("tasks.tasks.logger")
+def test_mark_expired_tasks_outer_exception(mock_logger, mock_get_expired_tasks):
+    """
+    Test handling of an outer exception in mark_expired_tasks.
+    """
+    mark_expired_tasks()
+
+    mock_logger.error.assert_called_with(
+        "An error occurred in mark_expired_tasks: Outer exception in mark_expired_tasks"
+    )
